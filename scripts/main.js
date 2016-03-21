a75c291f80ba0598307efc975776567ed60e015c
Will Cook <wellsee83@gmail.com>
2016-03-20 19:42:15 -0500

    Final commit

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,7 @@
+// Array to store item.
+var inputBank = [];
+//var totalItems = [];
+
 // Textbox to input item.
 var listerInputEl = document.getElementById('listerInput');
 // Button to submit item.
@@ -5,23 +9,28 @@ var addEl = document.getElementById('add');
 // Button to clear list.
 var clearEl = document.getElementById('clear');
 // List content
-var check = '<input type="checkbox"></input>\r\n'+ '<br>';
+//var check = '<input type="checkbox"></input>\r\n'+ '<br>';
 var listDisplayEl = document.getElementById('listDisplay');
-var storedListEl = document.querySelector('.storedList');
-// List item number.
-// Array to store item.
-var inputBank = [];
-var totalItems = [];
+//var storedListEl = document.querySelector('.storedList');
+
+//Render function 
+addEl.addEventListener('click', function render() {
+	//Clears html inside of section/array.
+	listDisplayEl.innerHTML = '';
+	//var itemCountEl = document.getElementById('itemCount').innerHTML+=check;
+	listDisplayEl.innerHTML= inputBank.join('<br> <br>');
+});
+clearEl.addEventListener('click', function() {
+	inputBank.splice(0);
+	listDisplayEl.innerHTML = '';
+})
 
 // Lister clicks button.
 function submitInput() {
-	//var counter = 0;
 	//Logs item.
 	console.log(listerInputEl.value);
 	//Stores item into array.
 	inputBank.push(listerInputEl.value);
-	//counter = (inputBank.length);
-	//totalItems.push(counter.toString() + ': ');
 	//Clears textbox for new item.
 	clearInput();
 };
@@ -30,16 +39,3 @@ function clearInput() {
 	listerInputEl.value = '';
 };
 
-//Render function 
-addEl.addEventListener('click', function render() {
-	//Clears html inside of section/array.
-	listDisplayEl.innerHTML = '';
-	var itemCountEl = document.getElementById('itemCount').innerHTML+=check;
-	listDisplayEl.innerHTML= inputBank.join('<br> <br>');
-});
-clearEl.addEventListener('click', function() {
-	inputBank.splice(0);
-	// totalItems.splice(0);
-	listDisplayEl.innerHTML = '';
-	//itemCountEl.innerHTML = '';
-})
